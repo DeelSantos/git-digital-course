24ba8467ac7ea1bb2caad27d8049bd286c2d8f9f
Salomao <augusto@salomaotech.com.br>
2024-12-02 20:42:48 -0300

    Manipulando para aprender

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function somar(num1, num2) {
 let soma = somar(10, 15)
 
 console.log(soma);
+
+let nomeDeAlunos = [
+    "Edelson",
+    "Carlos",
+    "Alberto",
+    "Juliana"
+]
+
+console.log(nomeDeAlunos[0]);
